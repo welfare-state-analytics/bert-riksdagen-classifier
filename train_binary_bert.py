e8664dc597f671453c1085c48e39a83d1a544cf7
ninpnin <vainoyrjanainen@icloud.com>
2023-10-19 16:56:28 +0200

    feat: save model so that it can be used in a huggingface pipeline

--- train_binary_bert.py
+++ train_binary_bert.py
@@ -9,10 +9,24 @@ import argparse
 from tqdm import tqdm
 import os
 
+# Logger go brrr pretty colors !
+# Don't mind me
+import sys, logging, colorlog
+TRAIN = 25
+LOG_COLORS = {'DEBUG':'cyan', 'INFO':'green', 'TRAIN':'blue', 'WARNING':'yellow', 'ERROR': 'red', 'CRITICAL':'red,bg_white'}
+logging.addLevelName(TRAIN, 'TRAIN')
+HANDLER = colorlog.StreamHandler(stream=sys.stdout)
+HANDLER.setFormatter(colorlog.ColoredFormatter('%(log_color)s%(asctime)s [%(levelname)s] %(white)s(%(name)s)%(reset)s: %(message)s',
+                                               log_colors=LOG_COLORS,
+                                               datefmt="%H:%M:%S",
+                                               stream=sys.stdout))
+LOGGER = colorlog.getLogger('finetuner')
+LOGGER.addHandler(HANDLER)
+LOGGER.setLevel(20)
+# Done with colors going brr
 
-def encode(df):
-    tokenizer = AutoTokenizer.from_pretrained('KBLab/bert-base-swedish-cased')
 
+def encode(df, tokenizer):
     # Tokenize all of the sentences and map the tokens to thier word IDs.
     input_ids = []
     attention_masks = []
@@ -67,8 +81,13 @@ def main(args):
     # Create binary label where seg = 1
     df['tag'] = np.where(df['seg'] == 1, 1, 0)
 
-    # Preprocess datasets
-    input_ids, attention_masks, labels = encode(df)
+    LOGGER.info("Load and save tokenizer...")
+    tokenizer = AutoTokenizer.from_pretrained('KBLab/bert-base-swedish-cased')
+    tokenizer.save_pretrained(args.model_filename)
+
+    
+    LOGGER.info("Preprocess datasets...")
+    input_ids, attention_masks, labels = encode(df, tokenizer)
 
     dataset = TensorDataset(input_ids, attention_masks, labels)
     train_size  = int(args.train_ratio * len(dataset))
@@ -98,9 +117,11 @@ def main(args):
             num_workers = args.num_workers
         )
 
+    LOGGER.info("Define model...")
     model = AutoModelForSequenceClassification.from_pretrained(
         'KBLab/bert-base-swedish-cased',
-        num_labels=2).to(args.device)
+        num_labels=2,
+        id2label={1: 'seg', 0: 'note'}).to(args.device)
 
     # Initialize optimizer
     loss_fn = nn.BCEWithLogitsLoss()
@@ -122,7 +143,9 @@ def main(args):
     best_valid_loss = float('inf')
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+
     for epoch in range(args.n_epochs):
+        LOGGER.log(TRAIN, f"Epoch {epoch} starts!")
         train_loss = 0
         model.train()
         for batch in tqdm(train_loader, total=len(train_loader)):
@@ -151,26 +174,24 @@ def main(args):
         train_loss_avg = train_loss * args.batch_size / len(train_loader)
         valid_loss_avg = valid_loss * args.batch_size / len(valid_loader)
 
-        print(f'Training Loss: {train_loss_avg:.3f}')
-        print(f'Validation Loss: {valid_loss_avg:.3f}')
+        LOGGER.log(TRAIN, f'Training Loss: {train_loss_avg:.3f}')
+        LOGGER.log(TRAIN, f'Validation Loss: {valid_loss_avg:.3f}')
 
         labels = df.loc[valid_dataset.indices, 'tag'].tolist()      
-        print(f'Eval accuracy: {sum([x==y for x, y in zip(labels, preds)]) / len(labels)}')
+        LOGGER.log(TRAIN, f'Eval accuracy: {sum([x==y for x, y in zip(labels, preds)]) / len(labels)}')
 
         # Store best model
-        if valid_loss < best_valid_loss:
-            best_valid_loss = valid_loss
-            torch.save({
-                'epoch': epoch+1,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'loss': loss_fn,
-                }, args.model_filename)
 
+        if valid_loss < best_valid_loss:
+            LOGGER.info("Best validation loss so far")
+            best_valid_loss = valid_loss
+            model.save_pretrained(args.model_filename)
+        else:
+            LOGGER.info("Not the best validation loss so far")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--model_filename", type=str, default="trained/binary_note_seg_model.pth")
+    parser.add_argument("--model_filename", type=str, default="trained/binary_note_seg_model")
     parser.add_argument("--data_folder", type=str, default="data/")
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--n_epochs", type=int, default=10)
