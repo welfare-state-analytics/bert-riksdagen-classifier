9d2931757aa09f3ea23a0b264d26f90508710331
maen1 <mk.maen93@gmail.com>
2024-03-06 11:39:57 +0100

    merge all combined files

--- preprocess/combine-files.py
+++ preprocess/combine-files.py
@@ -51,9 +51,41 @@ def combine_csv_files(directory):
 
 	print("Combined CSV file created:", output_file)
 
+
+
+def merge_combined_csv_files(folder_path):
+	# Initialize list to store combined rows
+	combined_rows = []
+	
+
+	# Output file path
+	output_file = folder_path + '/all-data.csv'
+	# Get all files in the folder that contain "combined" in their filename
+	combined_files = [file for file in os.listdir(folder_path) if "combined" in file]
+
+	# Loop through each combined file
+	for combined_file in combined_files:
+		with open(os.path.join(folder_path, combined_file), 'r', newline='') as csvfile:
+			reader = csv.DictReader(csvfile)
+			# Skip the header if not the first file
+			if combined_files.index(combined_file) != 0:
+				next(reader)
+			for row in reader:
+				combined_rows.append(row)
+
+	# Write combined rows to the output CSV file
+	with open(output_file, 'w', newline='') as csvfile:
+		writer = csv.DictWriter(csvfile, fieldnames=reader.fieldnames)
+		writer.writeheader()
+		writer.writerows(combined_rows)
+
+	print("CSV files merged into:", output_file)
+
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Combine CSV files from a specified directory.")
 	parser.add_argument("--dir", help="Path to the directory containing CSV files.")
 	args = parser.parse_args()
 
-	combine_csv_files(args.dir)
+	#combine_csv_files(args.dir)
+	merge_combined_csv_files(args.dir)
