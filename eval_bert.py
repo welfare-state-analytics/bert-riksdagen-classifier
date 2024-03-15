fe8cd24c363c0635d51a3d85dc11b97adcfa5b85
maen1 <mk.maen93@gmail.com>
2024-03-15 15:54:12 +0100

    wip

--- eval_bert.py
+++ eval_bert.py
@@ -1,21 +1,15 @@
-"""
-Train BERT-based classifier
-"""
-from transformers import AutoModel, AutoTokenizer, AutoModelForSequenceClassification
-from torch.utils.data import TensorDataset, random_split, DataLoader
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from torch.utils.data import TensorDataset, DataLoader
 from sklearn.metrics import accuracy_score, classification_report
-from transformers import get_linear_schedule_with_warmup
-from trainerlog import get_logger
-from bidict import bidict
-import torch.nn as nn
 from tqdm import tqdm
 import pandas as pd
 import argparse
 import logging
 import torch
 import os
+from bidict import bidict
 
-LOGGER = get_logger("eval-bert")
+LOGGER = logging.getLogger(__name__)
 
 def encode(df, tokenizer):
     # Tokenize all of the sentences and map the tokens to their word IDs.
@@ -25,14 +19,14 @@ def encode(df, tokenizer):
     # For every sentence...
     for ix, row in df.iterrows():
         encoded_dict = tokenizer.encode_plus(
-                            row['content'],                      
-                            add_special_tokens = True,
-                            max_length = 512,
-                            truncation=True,
-                            padding = 'max_length',
-                            return_attention_mask = True,
-                            return_tensors = 'pt',
-                       )
+            row['content'],
+            add_special_tokens=True,
+            max_length=512,
+            truncation=True,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt',
+        )
         
         # Add the encoded sentence to the list.    
         input_ids.append(encoded_dict['input_ids'])
@@ -57,9 +51,9 @@ def evaluate(model, loader):
         input_mask = batch[1].to(args.device)
         labels = batch[2].to(args.device)
         output = model(input_ids,
-            token_type_ids=None, 
-            attention_mask=input_mask, 
-            labels=labels)
+                       token_type_ids=None, 
+                       attention_mask=input_mask, 
+                       labels=labels)
         loss += output.loss.item()
         preds_batch = torch.argmax(output.logits, axis=1)
         batch_acc = torch.mean((preds_batch == labels).float())
@@ -75,7 +69,7 @@ def evaluate(model, loader):
 def main(args):
     os.makedirs(args.model_filename, exist_ok=True)
     logging.basicConfig(filename=f'{args.model_filename}/evaluation.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    df = pd.read_csv(f'{args.data_path}')
+    df = pd.read_csv(args.data_path)
     df = df.sample(frac=1, random_state=123).reset_index(drop=True)
 
     # Create binary label where seg = 1
@@ -95,7 +89,6 @@ def main(args):
     LOGGER.info("Load and tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained(args.model_filename)
 
-    
     LOGGER.info("Preprocess datasets...")
     input_ids, attention_masks, labels = encode(df, tokenizer)
 
@@ -104,18 +97,14 @@ def main(args):
     dataset = TensorDataset(input_ids, attention_masks, labels)
  
     test_loader = DataLoader(
-            dataset,
-            shuffle=False,
-            batch_size = args.batch_size,
-            num_workers = args.num_workers
-        )
+        dataset,
+        shuffle=False,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers
+    )
 
+    evaluate(model, test_loader)
 
-
-    eval(model, test_loader)
-
-
-    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
@@ -123,7 +112,7 @@ if __name__ == "__main__":
     parser.add_argument("--base_model", type=str, default="KBLab/bert-base-swedish-cased", help="Base model that the model is initialized from")
     parser.add_argument("--tokenizer", type=str, default="KBLab/bert-base-swedish-cased", help="Which tokenizer to use; accepts local and huggingface tokenizers.")
     parser.add_argument("--label_names", type=str, nargs="+", default=None, help="A list of label names to be used in the classifier. If None, takes class names from 'tag' column in the data.")
-    parser.add_argument("--data_path", type=str, default="data/training_data.csv", help="Testing data as a .CSV file. Needs to have 'content' (X) and 'tag' (Y) columns.")
+    parser.add_argument("--data_path", type=str, default="data/pilot/val_processed_data.csv", help="Testing data as a .CSV file. Needs to have 'content' (X) and 'tag' (Y) columns.")
     parser.add_argument("--device", type=str, default="cuda", help="Which device to use for training. Use 'cpu' for CPU.")
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--num_workers", type=int, default=4)
